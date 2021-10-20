717de4d486dd78dc134ef19564c2e484aa9a4387
OSH <llll_lll_lll@naver.com>
2021-10-21 02:04:12 +0900

    다이나믹 프록시 적용 예제 테스트

--- src/test/java/com/spring/toby/ProxyTest.java
+++ src/test/java/com/spring/toby/ProxyTest.java
@@ -3,9 +3,12 @@ package com.spring.toby;
 import com.spring.toby.proxy.Hello;
 import com.spring.toby.proxy.HelloTarget;
 import com.spring.toby.proxy.HelloUppercase;
+import com.spring.toby.proxy.UppercaseHandler;
 import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.lang.reflect.Proxy;
 
 public class ProxyTest {
   @Test
@@ -18,7 +21,7 @@ public class ProxyTest {
   }
 
   @Test
-  public void uppercase() {
+  public void helloUppercase() {
     Hello hello = new HelloUppercase(new HelloTarget());
 
     Assert.assertThat(hello.sayHello("Toby"), Is.is("HELLO TOBY"));
@@ -26,4 +29,16 @@ public class ProxyTest {
     Assert.assertThat(hello.sayThankYou("Toby"), Is.is("THANK YOU TOBY"));
   }
 
+  @Test
+  public void dynamicProxy() {
+    Hello proxiedHandler = (Hello) Proxy.newProxyInstance(
+            getClass().getClassLoader(),
+            new Class[] { Hello.class },
+            new UppercaseHandler(new HelloTarget())
+    );
+
+    Assert.assertThat(proxiedHandler.sayHello("Toby"), Is.is("HELLO TOBY"));
+    Assert.assertThat(proxiedHandler.sayHi("Toby"), Is.is("HI TOBY"));
+    Assert.assertThat(proxiedHandler.sayThankYou("Toby"), Is.is("THANK YOU TOBY"));
+  }
 }
