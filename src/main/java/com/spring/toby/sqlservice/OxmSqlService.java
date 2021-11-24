69c93fcde13baf7e6a24187a5c63775fec4efb97
OSH <llll_lll_lll@naver.com>
2021-11-25 05:52:33 +0900

    sqlService 에서 oxm 을 사용해 xml 을 파싱하도록 변경

---
+++ src/main/java/com/spring/toby/sqlservice/OxmSqlService.java
@@ -0,0 +1,66 @@
+package com.spring.toby.sqlservice;
+
+import com.spring.toby.independent.UserDao;
+import com.spring.toby.sqlservice.jaxb.SqlType;
+import com.spring.toby.sqlservice.jaxb.Sqlmap;
+import org.springframework.oxm.Unmarshaller;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+
+public class OxmSqlService implements SqlService {
+  private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
+  private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+
+  public void setSqlRegistry(SqlRegistry sqlRegistry) {
+    this.sqlRegistry = sqlRegistry;
+  }
+
+  public void setUnmarshaller(Unmarshaller unmarshaller) {
+    this.oxmSqlReader.setUnmarshaller(unmarshaller);
+  }
+
+  public void setSqlmapFile(String sqlmapFile) {
+    this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+  }
+
+  @PostConstruct
+  public void loadSql() {
+    this.oxmSqlReader.read(this.sqlRegistry);
+  }
+
+  @Override
+  public String getSql(String key) {
+    return this.sqlRegistry.findSql(key);
+  }
+
+
+  private class OxmSqlReader implements SqlReader {
+    private static final String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+    private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+    private Unmarshaller unmarshaller;
+
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+      this.unmarshaller = unmarshaller;
+    }
+
+    public void setSqlmapFile(String sqlmapFile) {
+      this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
+      try {
+        Source source = new StreamSource(UserDao.class.getClassLoader().getResourceAsStream(sqlmapFile));
+        Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(source);
+        for (SqlType sql : sqlmap.getSql()) {
+          sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
